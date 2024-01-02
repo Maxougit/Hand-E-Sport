eb736d975870ad6e51a236f46f34b7d0c05f2ef0
Maxougit <84478576+Maxougit@users.noreply.github.com>
2024-01-02 12:22:06 +0100

    Refactor social media integration cards

--- src/pages/reseaux.js
+++ src/pages/reseaux.js
@@ -10,14 +10,20 @@ export default function Integrations() {
         Nos réseaux sociaux
       </Typography>
 
-      <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="stretch"
+        gap={2}
+        sx={{ width: "100%" }}
+      >
         {/* Twitter Card */}
-        <Card>
+        <Card sx={{ flex: 1, minHeight: 300 }}>
           <CardContent>
             <Typography variant="h5" component="h2" my={2}>
               Twitter
             </Typography>
-            {/* https://help.twitter.com/en/using-x/how-to-embed-a-post */}
+            {/* Twitter Embed */}
             <Box
               dangerouslySetInnerHTML={{
                 __html: `<blockquote class="twitter-tweet"><p lang="zxx" dir="ltr"><a href="https://t.co/3BNnleMIhY">pic.twitter.com/3BNnleMIhY</a></p>&mdash; Out of Context Human Race (@NoContextHumans) <a href="https://twitter.com/NoContextHumans/status/1741791382702870971?ref_src=twsrc%5Etfw">January 1, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`,
@@ -27,15 +33,16 @@ export default function Integrations() {
         </Card>
 
         {/* Twitch Card */}
-        <Card>
+        <Card sx={{ flex: 1, minHeight: 300 }}>
           <CardContent>
             <Typography variant="h5" component="h2" my={2}>
               Twitch
             </Typography>
+            {/* Twitch Embed */}
             <iframe
               src={`https://player.twitch.tv/?channel=handesport_reims&parent=localhost`}
               height="300"
-              width="400"
+              width="100%"
               allowFullScreen={true}
               className="border-none"
             ></iframe>
@@ -43,7 +50,7 @@ export default function Integrations() {
         </Card>
 
         {/* Instagram Card */}
-        <Card>
+        <Card sx={{ flex: 1, minHeight: 300 }}>
           <CardContent>
             <Typography variant="h5" component="h2" my={2}>
               Instagram
