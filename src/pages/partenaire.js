3c99bc4139420a966714898168edf4a2dde16fed
Maxougit <84478576+Maxougit@users.noreply.github.com>
2024-01-13 15:33:37 +0100

    Refactor partenaires.js and partenaires.module.css

--- src/pages/partenaire.js
+++ src/pages/partenaire.js
@@ -1,21 +1,22 @@
 // pages/partenaires.js
-import Image from 'next/image';
-import styles from './partenaires.module.css'; // Assurez-vous de créer ce fichier CSS
-import BurgerMenu from '@/components/burgermenu';
+import Image from "next/image";
+import styles from "./partenaires.module.css"; // Assurez-vous de créer ce fichier CSS
+import BurgerMenu from "@/components/burgermenu";
+import { Typography } from "@material-ui/core";
 
 // Simulacre de données des partenaires
 const partenaires = [
   {
-    nom: 'Crous de Reims',
-    logo: '/PartenaireLogo/crous.png',
-    description: 'Financement et soutien',
-    lien: 'https://www.crous-reims.fr/'
+    nom: "Crous de Reims",
+    logo: "/PartenaireLogo/crous.png",
+    description: "Financement et soutien",
+    lien: "https://www.crous-reims.fr/",
   },
   {
-    nom: 'CVEC',
-    logo: '/PartenaireLogo/cvec.png',
-    description: 'Financement',
-    lien: 'https://cvec.etudiant.gouv.fr/'
+    nom: "CVEC",
+    logo: "/PartenaireLogo/cvec.png",
+    description: "Financement",
+    lien: "https://cvec.etudiant.gouv.fr/",
   },
   // Ajoutez d'autres partenaires ici...
 ];
@@ -23,16 +24,27 @@ const partenaires = [
 export default function Partenaires() {
   return (
     <div className={styles.container}>
-    <BurgerMenu />
-      <h1>Nos Partenaires</h1>
+      <BurgerMenu />
+      <Typography variant="h3" component="h1" my={1}>
+        Nos Partenaires
+      </Typography>
       <div className={styles.partenairesList}>
         {partenaires.map((partenaire, index) => (
           <div key={index} className={styles.partenaire}>
             <a href={partenaire.lien} target="_blank" rel="noopener noreferrer">
-              <Image src={partenaire.logo} alt={`Logo de ${partenaire.nom}`} width={150} height={150} />
+              <Image
+                src={partenaire.logo}
+                alt={`Logo de ${partenaire.nom}`}
+                width={150}
+                height={150}
+              />
             </a>
-            <h2>{partenaire.nom}</h2>
-            <p>{partenaire.description}</p>
+            <Typography variant="h6" my={1}>
+              {partenaire.nom}
+            </Typography>
+            <Typography variant="Typography">
+              {partenaire.description}
+            </Typography>
           </div>
         ))}
       </div>
