1014bfecc9dc013c83ffb218cb40bb6f1ae81ea5
Maxougit <84478576+Maxougit@users.noreply.github.com>
2024-01-13 15:33:22 +0100

    Restyle of burgermenu

--- src/components/burgermenu.js
+++ src/components/burgermenu.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import styles from './BurgerMenu.module.css'; // Assurez-vous de créer ce fichier CSS correspondant
+import React, { useState } from "react";
+import Link from "next/link";
+import "src/styles/globals.css";
+import styles from "./BurgerMenu.module.css"; // Assurez-vous de créer ce fichier CSS correspondant
 
 const BurgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={styles.burgerMenuContainer}>
-      <button className={`${styles.burgerButton} ${isOpen ? styles.open : ''}`} onClick={() => setIsOpen(!isOpen)}>
+      <button
+        className={`${styles.burgerButton} ${isOpen ? styles.open : ""}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <div className={styles.burgerIcon}>
           <div className={styles.burgerLine}></div>
           <div className={styles.burgerLine}></div>
           <div className={styles.burgerLine}></div>
         </div>
       </button>
-      <div className={`${styles.menuItems} ${isOpen ? styles.menuOpen : ''}`}>
-        <Link href="/" passHref><span className={styles.menuLink}>Accueil</span></Link>
-        <Link href="/galerie" passHref><span className={styles.menuLink}>Galerie</span></Link>
-        <Link href="/partenaire" passHref><span className={styles.menuLink}>Partenaires</span></Link>
-        <Link href="/reseaux" passHref><span className={styles.menuLink}>Réseaux</span></Link>
+      <div className={`${styles.menuItems} ${isOpen ? styles.menuOpen : ""}`}>
+        <Link href="/" passHref>
+          <span className={styles.menuLink}>Accueil</span>
+        </Link>
+        <Link href="/galerie" passHref>
+          <span className={styles.menuLink}>Galerie</span>
+        </Link>
+        <Link href="/partenaire" passHref>
+          <span className={styles.menuLink}>Partenaires</span>
+        </Link>
+        <Link href="/reseaux" passHref>
+          <span className={styles.menuLink}>Réseaux</span>
+        </Link>
       </div>
     </div>
   );
